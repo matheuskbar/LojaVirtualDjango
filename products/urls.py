aa9fb902c5bd06c7f0717942ee5aaa551683c106
DESKTOP-N7HHFNK\Matheus <matheuskbar@gmail.com>
2021-10-26 13:57:53 -0200

    Pagina produtos

---
+++ products/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from products.views import ProductsList
+
+app_name = 'products'
+
+urlpatterns = [
+    path('', ProductsList.as_view(), name='product-list'),
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
-# Create your views here.
+from products.models import Product
+
+
+class ProductDetail(DetailView):
+    pass
+
+
+class ProductsList(ListView):
+
+    model = Product
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
