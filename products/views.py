831d601ef62f070c77b21484a4d28e8539aa09e1
DESKTOP-N7HHFNK\Matheus <matheuskbar@gmail.com>
2021-10-26 17:21:36 -0200

    no message

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
-from products.models import Product
+from products.models import Product, Category
 
 
 class ProductDetail(DetailView):
