4089af4147422d044334985ac94e3992ac2e8eca
DESKTOP-N7HHFNK\Matheus <matheuskbar@gmail.com>
2021-11-01 13:06:46 -0200

    no message

--- cart/cart.py
+++ cart/cart.py
@@ -2,18 +2,28 @@
 
 class Cart:
     def __init__(self, request):
-        if request.session.get['cart'] in None:
+        if request.session.get('cart') in None:
             request.session['cart'] = {}
 
         self.cart = request.session['cart']
         self.session = request.session
 
-    def add(self, product, quantity=1):
+    def add(self, product, quantity=1, override_quantity=False):
         product_id = str(product.id)
 
         if product_id not in self.cart:
             self.cart[product_id] = {
-                'quantity': 1,
-                'price': str(product.price)
+                'quantity': 0,
+                'price': str(product.price),
+                'total_price': str(product.price),
             }
+            if override_quantity:
+                self.cart[product_id]['quantity'] = quantity
+                self.cart[product_id]['total_price'] = str(product.price * quantity)
+            else:
+                self.cart[product_id]['quantity'] += 1
+
+        self.save()
+
+    def save(self):
         self.session.modified = True
--- cart/views.py
+++ cart/views.py
@@ -2,19 +2,25 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_POST
 
 from cart.cart import Cart
+from cart.forms.cart_add_form import CartAddProductForm
 from products.models import Product
 
 
 def cart_detail(request):
     return render(request, 'cart/cart_detail.html')
 
+
 @require_POST
 def cart_add(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
-
-    cart.add(
-        product=product
-    )
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        form.save()
+        cart.add(
+            product=product,
+            quantity=form.quantity,
+            override_quantity=form.override
+        )
 
     return redirect("cart:detail")
